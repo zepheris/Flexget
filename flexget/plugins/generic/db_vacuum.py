e9a0078de081347a56f1b85c4efb7addcb0254cf
liiight <or.carmi82@gmail.com>
2017-01-16 10:36:08 +0200

    [fix] db vacuum - Moved vacuum to its own hook. Fixes  #1596

--- flexget/plugins/generic/db_vacuum.py
+++ flexget/plugins/generic/db_vacuum.py
@@ -4,6 +4,7 @@ from builtins import *  # noqa pylint: disable=unused-import, redefined-builtin
 import logging
 from datetime import datetime, timedelta
 
+from flexget.manager import Session
 from flexget.utils.simple_persistence import SimplePersistence
 from flexget.event import event
 
@@ -12,12 +13,13 @@ VACUUM_INTERVAL = timedelta(weeks=24)  # 6 months
 
 
 # Run after the cleanup is actually finished, but before analyze
-@event('manager.db_cleanup', 1)
-def on_cleanup(manager, session):
+@event('manager.db_vacuum', 1)
+def on_cleanup(manager):
     # Vacuum can take a long time, and is not needed frequently
     persistence = SimplePersistence('db_vacuum')
     last_vacuum = persistence.get('last_vacuum')
     if not last_vacuum or last_vacuum < datetime.now() - VACUUM_INTERVAL:
         log.info('Running VACUUM on database to improve performance and decrease db size.')
-        session.execute('VACUUM')
+        with Session() as session:
+            session.execute('VACUUM')
         persistence['last_vacuum'] = datetime.now()
